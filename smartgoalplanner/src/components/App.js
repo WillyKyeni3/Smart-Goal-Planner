6714adeff7dc865ec4da28f48b8c6425d7c043f8
willymusyoka <willykyeni@gmail.com>
2025-07-20 12:54:36 +0300

    Imported the componets from the specified path.

--- smartgoalplanner/src/components/App.js
+++ smartgoalplanner/src/components/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import GoalOverview from './GoalOverview';
+import GoalForm from './GoalForm';
+import GoalList from './GoalList';
+import GoalItem from './GoalItem';
 
 function App() {
   return (
@@ -7,6 +10,15 @@ function App() {
       <h1>Smart Goal Planner</h1>
       <p>Your journey to achieving SMART goals starts here!</p>
         <GoalOverview />
+        <GoalForm />
+        <GoalList />
+        <GoalItem goal={{
+        //   name: "Save for Vacation",
+        //   targetAmount: 2000,
+        //   savedAmount: 500,
+        //   category: "Travel",
+        //   deadline: "2024-12-31"
+        }} />
     </div>
   );
 }
