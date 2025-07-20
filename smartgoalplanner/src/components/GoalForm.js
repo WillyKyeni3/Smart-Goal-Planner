009065ce18d9116bc6c62b21f1352d66452f7984
willymusyoka <willykyeni@gmail.com>
2025-07-20 14:32:37 +0300

    Adds required to input

--- smartgoalplanner/src/components/GoalForm.js
+++ smartgoalplanner/src/components/GoalForm.js
@@ -4,10 +4,10 @@ function GoalForm() {
   return (
     <form className="goal-form">
       <h3>Create New Goal</h3>
-      <input placeholder="Goal name" />
-      <input placeholder="Target amount" type="number" />
-      <input placeholder="Category" />
-      <input type="date" />
+      <input placeholder="Goal name" required />
+      <input placeholder="Target amount" type="number" required />
+      <input placeholder="Category" required />
+      <input type="date" required />
       <button type="submit">Add Goal</button>
     </form>
   );
