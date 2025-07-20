d1a646b1b2e7016813cb32d45a8c78db56665b16
willymusyoka <willykyeni@gmail.com>
2025-07-20 14:32:16 +0300

    Remeved Goal item

--- smartgoalplanner/src/components/App.js
+++ smartgoalplanner/src/components/App.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import GoalOverview from './GoalOverview';
 import GoalForm from './GoalForm';
 import GoalList from './GoalList';
-// import GoalItem from './GoalItem';
 
 function App() {
     const [goals, setGoals] = useState([]);
@@ -14,13 +13,6 @@ function App() {
         <GoalOverview goals={goals}/>
         <GoalForm />
         <GoalList goals={goals} />
-        {/* <GoalItem goal={{
-        //   name: "Save for Vacation",
-        //   targetAmount: 2000,
-        //   savedAmount: 500,
-        //   category: "Travel",
-        //   deadline: "2024-12-31"
-        }} /> */}
     </div>
   );
 }
