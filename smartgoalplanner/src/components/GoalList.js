d53cbb8ab271b6b3ab3fae936dbbbbc02e3acae3
willymusyoka <willykyeni@gmail.com>
2025-07-20 12:52:24 +0300

    Added stater code of the list

---
+++ smartgoalplanner/src/components/GoalList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function GoalList() {
+  return (
+    <div className="goal-grid">
+        <h2>🏆 Your Goals</h2>
+      {/* GoalItem components will go here */}
+    </div>
+  );
+}
+
+export default GoalList;
