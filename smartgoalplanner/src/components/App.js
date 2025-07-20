e32fc3d52292e695fdb6bd28aa9fa4d1ce9e5cf4
willymusyoka <willykyeni@gmail.com>
2025-07-20 12:36:30 +0300

    Imported GoalOverview component from the correct path

---
+++ smartgoalplanner/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import GoalOverview from './GoalOverview';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Smart Goal Planner</h1>
+      <p>Your journey to achieving SMART goals starts here!</p>
+        <GoalOverview />
+    </div>
+  );
+}
+
+export default App;
