3d905bc093f4fbc6252ed6b508d0e91b68acd00c
willymusyoka <willykyeni@gmail.com>
2025-07-20 14:17:25 +0300

    Made Gaol item child of Goal list

--- smartgoalplanner/src/components/App.js
+++ smartgoalplanner/src/components/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import GoalOverview from './GoalOverview';
 import GoalForm from './GoalForm';
 import GoalList from './GoalList';
-import GoalItem from './GoalItem';
+// import GoalItem from './GoalItem';
 
 function App() {
     const [goals, setGoals] = useState([]);
@@ -14,13 +14,13 @@ function App() {
         <GoalOverview goals={goals}/>
         <GoalForm />
         <GoalList goals={goals} />
-        <GoalItem goal={{
+        {/* <GoalItem goal={{
         //   name: "Save for Vacation",
         //   targetAmount: 2000,
         //   savedAmount: 500,
         //   category: "Travel",
         //   deadline: "2024-12-31"
-        }} />
+        }} /> */}
     </div>
   );
 }
--- smartgoalplanner/src/components/GoalList.js
+++ smartgoalplanner/src/components/GoalList.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import GoalItem from './GoalItem';
 
 function GoalList({ goals }) {
   return (
     <div className="goal-grid">
-        <h2>🏆 Your Goals</h2>
+      <h2>🏆 Your Goals</h2>
+      <GoalItem 
+        goals={goals}
+      />
       {/* GoalItem components will go here */}
     </div>
   );
 }
 
 export default GoalList;
+
+// {
+//           name: "Save for Vacation",
+//           targetAmount: 2000,
+//           savedAmount: 500,
+//           category: "Travel",
+//           deadline: "2024-12-31"}
